5b8a292f4ee471b9422a4128872d444bd152155f
jamesburns22 <james@thedogslanding.com>
2022-07-09 01:26:33 +0300

    main-done

--- script.js
+++ script.js
@@ -3,11 +3,50 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 let apiQuotes = [];
+
+// Show Loading
+
+const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading 
+
+const complete = () => {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show New Quote
+const newQuote = () => {
+    loading();
+
+    // pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // check if authorfield is blank and replace it with 'unknown'
+    if (!quote.author) {
+        authorText.textContent = 'unknown'
+    } else {
+        authorText.textContent = quote.author;
+    };
+    // Check Quote Length to determine styling
+
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote')
+    } else (
+        quoteText.classList.remove('long-quote')
+    )
+    // Set Quote, Hide Loader
+    complete();
+    quoteText.textContent = quote.text;
+}
 
 // Get quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
     try {
         const response = await fetch(apiUrl);
@@ -18,29 +57,6 @@ async function getQuotes() {
         // Catch Error Here
     }
 }
-
-// Show New Quote
-const newQuote = () => {
-    // pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // check if authorfield is blank and replace it with 'unknown'
-    if (!quote.author) {
-        authorText.textContent = 'unknown'
-    } else {
-        authorText.textContent = quote.author;
-    };
-
-    // Check Quote Length to determine styling
-
-    if (quote.text.length > 50) {
-        quoteText.classList.add('long-quote')
-    } else (
-        quoteText.classList.remove('long-quote')
-    )
-
-    quoteText.textContent = quote.text;
-}
-
 
 //tweet quote 
 const tweetQuote = () => {
@@ -58,4 +74,3 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
 // newQuote();
-
